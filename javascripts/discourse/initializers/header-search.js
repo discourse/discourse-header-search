6933f4c5f3ef865de27073c6e71ce94046905dae
Kris <kris.aubuchon@discourse.org>
2022-08-23 21:12:23 -0400

    UX: hide from logged out users when relevant (#8)

--- javascripts/discourse/initializers/header-search.js
+++ javascripts/discourse/initializers/header-search.js
@@ -13,7 +13,13 @@ export default {
           {{header-topic-info attrs=attrs}}
         {{else}}
           {{#unless this.site.mobileView}}
-            {{floating-search-input attrs=attrs}}
+            {{#if this.site.siteSettings.login_required}}
+              {{#if this.currentUser}}
+                {{floating-search-input attrs=attrs}}
+              {{/if}}
+            {{else}}
+              {{floating-search-input attrs=attrs}}
+            {{/if}}   
           {{/unless}}
         {{/if}}
         <div class="panel clearfix">{{yield}}</div>
