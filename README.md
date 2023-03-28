a2518e7eb5312048ff7d00f652a3d034de9de5f7
Penar Musaraj <pmusaraj@gmail.com>
2023-03-28 07:51:14 -0400

    FEATURE: optional external search icons (#19)

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 This component removes the search icon from the header menu and creates a search bar which is placed in the middle of the header area.
 
 ![docs card filter preview](./readme-assets/example.png)
+
+### Optionally show external search icons
+
+Use the `extra_search_icons` to show additional icons in the search bar.
+
+This setting supports the following params:
+
+- `prefix`: the prefix to the search URL when clicking the respective icon
+- `icon`: icon to show, from the Font Awesome set (may need to be added to the `svg icons` setting as well)
+- `target`: optionally set to "\_blank" to open the link in its own tab/window by default
+- `showInCategories`: a list of category IDs on which to show this specific icon (if not present, icon is shown everywhere)
---
+++ test/acceptance/header-search-test.js
@@ -0,0 +1,95 @@
+import {
+  acceptance,
+  query,
+  visible,
+} from "discourse/tests/helpers/qunit-helpers";
+import { click, fillIn, visit } from "@ember/test-helpers";
+import { test } from "qunit";
+
+acceptance("Header Search", function () {
+  test("shows in header", async function (assert) {
+    await visit("/");
+
+    assert.ok(
+      visible(".floating-search-input"),
+      "search form present in header"
+    );
+    assert.ok(visible("#search-term"), "it shows the search input");
+    assert.ok(
+      visible(".show-advanced-search"),
+      "it shows full page search button"
+    );
+
+    await click(".floating-search-input #search-term");
+    await fillIn(".floating-search-input #search-term", "test");
+
+    assert.ok(visible(".search-menu .results"), "it has results");
+  });
+});
+
+acceptance("Header Search - Extra Icons", function (needs) {
+  const itemsJSON = [
+    {
+      name: "google",
+      params: [
+        {
+          name: "icon",
+          value: "fab-google",
+        },
+        {
+          name: "prefix",
+          value: "https://www.google.com/search?q=",
+        },
+      ],
+    },
+  ];
+  needs.hooks.beforeEach(() => {
+    settings.extra_search_icons = JSON.stringify(itemsJSON);
+  });
+
+  needs.hooks.afterEach(() => {
+    settings.extra_search_icons = "[]";
+  });
+
+  test("shows additional icons", async function (assert) {
+    await visit("/");
+
+    assert.ok(
+      visible(".floating-search-input"),
+      "search form present in header"
+    );
+
+    await click(".floating-search-input #search-term");
+    await fillIn(".floating-search-input #search-term", "test");
+
+    assert.ok(
+      visible(".floating-search-input .extra-search-icons"),
+      "it has extra icons"
+    );
+
+    assert.ok(
+      visible(
+        ".floating-search-input .extra-search-icons .search-extra-icon-google"
+      ),
+      "it has the google search icon (as defined in theme settings)"
+    );
+
+    assert.strictEqual(
+      query(
+        ".floating-search-input .extra-search-icons .search-extra-icon-google"
+      ).href,
+      "https://www.google.com/search?q=test",
+      "it appends current search term to external link"
+    );
+
+    assert.ok(
+      visible(".show-advanced-search"),
+      "it still shows full page search button"
+    );
+
+    assert.notOk(
+      visible(".floating-search-input .clear-search"),
+      "it no longer shows the clear search button"
+    );
+  });
+});
