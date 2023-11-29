8617914837758afd32bf7f3c1c3f45393ad2690f
Jarek Radosz <jradosz@gmail.com>
2023-11-29 23:04:25 +0100

    DEV: Update linting (#32)

--- test/acceptance/header-search-test.js
+++ test/acceptance/header-search-test.js
@@ -1,10 +1,10 @@
+import { click, fillIn, visit } from "@ember/test-helpers";
+import { test } from "qunit";
 import {
   acceptance,
   query,
   visible,
 } from "discourse/tests/helpers/qunit-helpers";
-import { click, fillIn, visit } from "@ember/test-helpers";
-import { test } from "qunit";
 
 acceptance("Header Search", function () {
   test("shows in header", async function (assert) {
