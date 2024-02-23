2734f7099e3205d99405bbe8a7e3fd688eece54d
Jarek Radosz <jarek@cvx.dev>
2024-02-24 00:17:15 +0100

    Update search-bar-icons.js

--- javascripts/discourse/components/search-bar-icons.js
+++ javascripts/discourse/components/search-bar-icons.js
@@ -1,6 +1,6 @@
 import Component from "@glimmer/component";
+import { getOwner } from "@ember/application";
 import { inject as service } from "@ember/service";
-import { getOwner } from "@ember/application"
 
 export default class SearchBarIcons extends Component {
   @service router;
