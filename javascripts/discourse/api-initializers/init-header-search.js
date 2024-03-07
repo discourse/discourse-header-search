887c57b2c50ec5825e68aac622eebe7150c37656
Jordan Vidrine <jordan@jordanvidrine.com>
2024-03-07 14:16:25 -0600

    prettier

--- javascripts/discourse/api-initializers/init-header-search.js
+++ javascripts/discourse/api-initializers/init-header-search.js
@@ -2,8 +2,5 @@ import { apiInitializer } from "discourse/lib/api";
 import HeaderSearch from "../components/header-search";
 
 export default apiInitializer("1.14.0", (api) => {
-  api.renderInOutlet(
-    "before-header-panel",
-    HeaderSearch
-  );
+  api.renderInOutlet("before-header-panel", HeaderSearch);
 });
