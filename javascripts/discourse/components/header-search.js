57538b86ae06c1ea01a0bacac07b3c51a433d3c0
awesomerobot <kris.aubuchon@discourse.org>
2024-03-20 13:23:11 -0400

    prettier

--- javascripts/discourse/components/header-search.js
+++ javascripts/discourse/components/header-search.js
@@ -16,10 +16,6 @@ export default class HeaderSearch extends Component {
   get shouldDisplay() {
     const titleDocked = this.args.outletArgs?.topic;
 
-    return (
-      this.displayForUser &&
-      !this.site.mobileView &&
-      !titleDocked
-    );
+    return this.displayForUser && !this.site.mobileView && !titleDocked;
   }
 }
