41d2ed22fc62b2d13f971def290b3d3ff5c6ce1f
Loïc Guitaut <loic@discourse.org>
2025-04-08 14:57:32 +0200

    DEV: Add system spec to check core features are working fine

--- javascripts/discourse/components/header-search.js
+++ javascripts/discourse/components/header-search.js
@@ -1,16 +1,16 @@
 import Component from "@glimmer/component";
 import { service } from "@ember/service";
 import { modifier as modifierFn } from "ember-modifier";
-import { focusSearchInput } from "discourse/components/search-menu";
 
 export default class HeaderSearch extends Component {
   @service site;
   @service siteSettings;
   @service currentUser;
   @service appEvents;
+  @service search;
 
   handleKeyboardShortcut = modifierFn(() => {
-    const cb = () => focusSearchInput();
+    const cb = () => this.search.focusSearchInput();
     this.appEvents.on("header:keyboard-trigger", cb);
     return () => this.appEvents.off("header:keyboard-trigger", cb);
   });
--- spec/system/keyboard_shortcut_spec.rb
+++ spec/system/keyboard_shortcut_spec.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-RSpec.describe "header search keyboard shortcut", system: true do
-  let!(:theme) { upload_theme_component }
+RSpec.describe "header search keyboard shortcut", type: :system do
+  before { upload_theme_component }
 
   it "focuses search header when keyboard shortcut used" do
     visit("/")
-    expect(page).to have_css("#search-term", focused: false)
+    expect(page).to have_css(".search-term__input", focused: false)
     find("body").send_keys("/")
-    expect(page).to have_css("#search-term", focused: true)
+    expect(page).to have_css(".search-term__input", focused: true)
   end
 end
